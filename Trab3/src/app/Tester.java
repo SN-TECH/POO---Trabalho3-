83d6ad9e5ce6d6e7cad2dc1cac6226666d3265fb
Michael <michael.souza@soulasalle.com.br>
2020-04-13 21:12:27 -0300

    structure updated

---
+++ Trab3/src/app/Tester.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Tester {
+    public static void main(String[] args) throws Exception {
+        System.out.println("*************************************************");
+        System.out.println("\nNome: Michaeel Nascimento --- Matrícula: 15719");
+        System.out.println("\n***********************************************");
+    }
+}
